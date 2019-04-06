50d7de798b628e39fe48511c2bae0f6206344782
Wei Zhang <weizhang@avana.asia>
2019-04-06 16:42:44 +0800

    add test for get device type

--- tests/DeviceCheckTest.php
+++ tests/DeviceCheckTest.php
@@ -22,4 +22,44 @@ class DeviceCheckTest extends TestCase
         $this->expectException(\Exception::class);
         $deviceCheck->setServerVar([])->isMobile();
     }
+
+    /** @test */
+    public function test_get_device_type_is_desktop()
+    {
+        $deviceCheck = new DeviceCheck();
+        $deviceCheck->setServerVar([
+            'HTTP_USER_AGENT' => 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0',
+            'HTTP_ACCEPT' => 'text/plain; q=0.5, text/html,text/x-dvi; q=0.8, text/x-c'
+        ]);
+        $deviceType = $deviceCheck->deviceType();
+
+        $this->assertEquals($deviceType, 'desktop');
+    }
+    
+    /** @test */
+    public function test_get_device_type_is_mobile()
+    {
+        $deviceCheck = new DeviceCheck();
+
+        $deviceCheck->setServerVar([
+            'HTTP_USER_AGENT' => 'Mozilla/5.0 (Linux; <Android Version>; <Build Tag etc.>) AppleWebKit/<WebKit Rev> (KHTML, like Gecko) Chrome/<Chrome Rev> Mobile Safari/<WebKit Rev>',
+            'HTTP_ACCEPT' => 'text/plain; q=0.5, text/html,text/x-dvi; q=0.8, text/x-c'
+        ]);
+        $deviceType = $deviceCheck->deviceType();
+
+        $this->assertEquals($deviceType, 'mobile');    
+    }
+
+    /** @test */
+    public function test_get_device_type_is_tablet()
+    {
+        $deviceCheck = new DeviceCheck();
+        $deviceCheck->setServerVar([
+            'HTTP_USER_AGENT' => 'Mozilla/5.0 (Linux; <Android Version>; <Build Tag etc.>) AppleWebKit/<WebKit Rev>(KHTML, like Gecko) Chrome/<Chrome Rev> Safari/<WebKit Rev>',
+            'HTTP_ACCEPT' => 'text/plain; q=0.5, text/html,text/x-dvi; q=0.8, text/x-c'
+        ]);
+        $deviceType = $deviceCheck->deviceType();
+        
+        $this->assertEquals($deviceType, 'tablet');
+    }
 }
